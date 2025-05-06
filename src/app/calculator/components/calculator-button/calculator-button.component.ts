05c9e48d006ba7118d1ae812c2aa4e8351495b9d
Nick Paz <31939425+nickpazfernande@users.noreply.github.com>
2025-05-06 08:43:29 -0300

    Input Signals Y HostBindings

--- src/app/calculator/components/calculator-button/calculator-button.component.ts
+++ src/app/calculator/components/calculator-button/calculator-button.component.ts
@@ -1,4 +1,4 @@
-import { Attribute, ChangeDetectionStrategy, Component } from '@angular/core';
+import { Attribute, ChangeDetectionStrategy, Component, HostBinding, input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'calculator-button',
@@ -7,6 +7,20 @@ import { Attribute, ChangeDetectionStrategy, Component } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: {
     class: 'w-1/4 border-r border-b border-indigo-400'
-  }
+  },
+  styleUrls: ['./calculator-button.component.css'],
 })
-export class CalculatorButtonComponent { }
+
+
+export class CalculatorButtonComponent {
+  //Input signals
+  public isCommand = input(false, {
+    transform: (value: boolean | string) => {
+      return typeof value === 'string' ? value === '' : value
+    }
+  });
+
+  @HostBinding('class.is-command') get commandStyle() {
+    return this.isCommand();
+  }
+}
